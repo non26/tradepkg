f206681a633185c5f27062b300bbc0e73eb4de61
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2024-11-24 22:19:53 +0700

    fix: method to support usecase

--- pkg/bn/sign_request.go
+++ pkg/bn/sign_request.go
@@ -7,47 +7,67 @@ import (
 	"fmt"
 	"net/url"
 	"reflect"
+	"slices"
 )
 
 type IBinanceSignature[T any] interface {
-	Sign(payload []byte, key []byte) string
-	CreateBinanceSignature(data *url.Values) string
-	GetQueryStringFromStructType(m *T) url.Values
+	GetSignature(key []byte) string
+	GetQueryStringBinanceSignature() string
+	CreateQueryStringFromPayload(m *T) url.Values
 }
 
 type binanceSignature[T any] struct {
-	apiKey    string
-	secretKey string
+	apiKey         string
+	secretKey      string
+	bnsign         string
+	exclude_fields []string
+	encodepayload  string
 }
 
 func NewBinanceNonWsSignature[T any](apiKey, secretKey string) IBinanceSignature[T] {
 	return &binanceSignature[T]{
-		apiKey:    apiKey,
-		secretKey: secretKey,
+		apiKey:         apiKey,
+		secretKey:      secretKey,
+		exclude_fields: []string{},
 	}
 }
 
-func (b *binanceSignature[T]) GetQueryStringFromStructType(m *T) url.Values {
+func (b *binanceSignature[T]) CreateQueryStringFromPayload(m *T) url.Values {
 	st := reflect.TypeOf(m).Elem()
 	v := reflect.ValueOf(m).Elem()
 	q := url.Values{}
 	for i := 0; i < st.NumField(); i++ {
 		field := st.Field(i).Tag.Get("json")
-		value := v.FieldByIndex([]int{i}).String()
-		q.Add(field, value)
+		if !b.isExcludeField(field) {
+			value := v.FieldByIndex([]int{i}).String()
+			q.Add(field, value)
+		}
 	}
+	b.encodepayload = q.Encode()
 	return q
 }
 
-func (b *binanceSignature[T]) CreateBinanceSignature(data *url.Values) string {
-	payload := data.Encode()
-	encodeString := b.Sign([]byte(payload), []byte(b.secretKey))
-	encodeData := fmt.Sprintf("%v&signature=%v", data.Encode(), encodeString)
+func (b *binanceSignature[T]) isExcludeField(field string) bool {
+	return slices.Contains(b.exclude_fields, field)
+}
+
+func (b *binanceSignature[T]) SetExcludeFields(fields []string) {
+	b.exclude_fields = fields
+}
+
+func (b *binanceSignature[T]) SetExcludeField(field string) {
+	b.exclude_fields = append(b.exclude_fields, field)
+}
+
+func (b *binanceSignature[T]) GetQueryStringBinanceSignature() string {
+	encodeData := fmt.Sprintf("%v&signature=%v", b.encodepayload, b.bnsign)
 	return encodeData
 }
 
-func (b *binanceSignature[T]) Sign(payload []byte, key []byte) string {
+func (b *binanceSignature[T]) GetSignature(key []byte) string {
+	payload := []byte(b.encodepayload)
 	mac := hmac.New(sha256.New, key)
 	mac.Write(payload)
-	return hex.EncodeToString(mac.Sum(nil))
+	b.bnsign = hex.EncodeToString(mac.Sum(nil))
+	return b.bnsign
 }
