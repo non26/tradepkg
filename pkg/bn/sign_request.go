a289d052c193d20f9f6f8b23db3d1633e709e137
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2024-11-25 15:42:16 +0700

    feat: add signature for ws

--- pkg/bn/sign_request.go
+++ pkg/bn/sign_request.go
@@ -1,8 +1,10 @@
 package bn
 
 import (
+	"crypto/ed25519"
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 	"net/url"
@@ -12,6 +14,9 @@ import (
 
 type IBinanceSignature[T any] interface {
 	GetSignature() string
+	GetWSSignature() string
+	GetEncodePayload() string
+	GetURLValue() url.Values
 	GetQueryStringBinanceSignature() string
 	CreateQueryStringFromPayload(m *T) *binanceSignature[T]
 	SetExcludeFields(fields []string)
@@ -24,6 +29,7 @@ type binanceSignature[T any] struct {
 	bnsign         string
 	exclude_fields []string
 	encodepayload  string
+	urlValue       url.Values
 }
 
 func NewBinanceNonWsSignature[T any](apiKey, secretKey string) IBinanceSignature[T] {
@@ -45,6 +51,7 @@ func (b *binanceSignature[T]) CreateQueryStringFromPayload(m *T) *binanceSignatu
 			q.Add(field, value)
 		}
 	}
+	b.urlValue = q
 	b.encodepayload = q.Encode()
 	return b
 }
@@ -72,4 +79,18 @@ func (b *binanceSignature[T]) GetSignature() string {
 	mac.Write(payload)
 	b.bnsign = hex.EncodeToString(mac.Sum(nil))
 	return b.bnsign
+}
+
+func (b *binanceSignature[T]) GetWSSignature() string {
+	privateKey := ed25519.PrivateKey(b.secretKey)
+	signature := ed25519.Sign(privateKey, []byte(b.encodepayload))
+	base64Signature := base64.StdEncoding.EncodeToString(signature)
+	return base64Signature
+}
+
+func (b *binanceSignature[T]) GetEncodePayload() string {
+	return b.encodepayload
+}
+func (b *binanceSignature[T]) GetURLValue() url.Values {
+	return b.urlValue
 }
