b9a58b9acdbfd7aed4c37cf6b160763433163f56
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2024-11-28 11:09:23 +0700

    feat: implement binance client

---
+++ pkg/bn/binance_client/binance_client.go
@@ -0,0 +1,38 @@
+package binanceclient
+
+import "net/http"
+
+type IBinanceSerivceHttpClient interface {
+	Do(bnreq *http.Request) error
+	GetBinanceHttpClientResponse() *http.Response
+	SetClient(t *http.Transport)
+}
+
+type binanceSerivceHttpClient struct {
+	client   *http.Client
+	response *http.Response
+}
+
+func NewBinanceSerivceHttpClient() IBinanceSerivceHttpClient {
+	binanceClient := binanceSerivceHttpClient{}
+	return &binanceClient
+}
+
+func (b *binanceSerivceHttpClient) SetClient(t *http.Transport) {
+	b.client = &http.Client{
+		Transport: t,
+	}
+}
+
+func (b *binanceSerivceHttpClient) Do(bnreq *http.Request) error {
+	res, err := b.client.Do(bnreq)
+	if err != nil {
+		return err
+	}
+	b.response = res
+	return nil
+}
+
+func (b *binanceSerivceHttpClient) GetBinanceHttpClientResponse() *http.Response {
+	return b.response
+}
