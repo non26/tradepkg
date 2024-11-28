609dbc8213c34912eed5ffe4f322661f0e2071bb
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2024-11-28 11:26:10 +0700

    feat: binance transport

---
+++ pkg/bn/binane_transport/binance_transport.go
@@ -0,0 +1,22 @@
+package binanetransport
+
+import "net/http"
+
+type IBinanceServiceHttpTransport interface {
+	GetTransport() *http.Transport
+}
+
+type binanceServiceHttpTransport struct {
+	t *http.Transport
+}
+
+func NewBinanceTransport(t *http.Transport) *binanceServiceHttpTransport {
+	b := binanceServiceHttpTransport{
+		t: t,
+	}
+	return &b
+}
+
+func (b *binanceServiceHttpTransport) GetTransport() *http.Transport {
+	return b.t
+}
