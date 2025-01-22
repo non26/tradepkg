d02c5241e3126c47c4b3c0cb5404c6a672a330e3
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2025-01-22 23:15:11 +0700

    add default client method

--- pkg/bn/utils/bn_clientId.go
+++ pkg/bn/utils/bn_clientId.go
@@ -1,7 +1,16 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// default client id with running number from symbol
 func BinanceDefaultClientID(symbol string, position_side string, counting int) string {
 	return fmt.Sprintf("%s_%s_%d", symbol, position_side, counting)
+}
+
+// default client id without running number from symbol but got unix nano instead
+func BinanceDefaultClientIDV2(symbol string, position_side string) string {
+	return fmt.Sprintf("%s_%s_%d", symbol, position_side, time.Now().UnixNano())
 }
